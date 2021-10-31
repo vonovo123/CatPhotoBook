62364b2cc05d422bda4520fabe25dabcdff218fc
dynamic-henry-kwon <jukdo67@gmail.com>
2021-10-31 12:26:27 +0900

    개발완료

--- src/main.js
+++ src/main.js
@@ -1,27 +1,7 @@
 import App from './App.js';
-import About from './About.js';
 
 const loadApp = () => {
   const app = new App(document.querySelector('#app'));
   app.render();
 };
 loadApp();
-
-const aboutBtn = document.querySelector('#about');
-const appMainBtn = document.querySelector('#appMain');
-aboutBtn.addEventListener('click', () => {
-  history.pushState({}, '', '/about');
-  if (document.querySelector('.app')) {
-    document.querySelector('.app').remove();
-    const app = new About(document.querySelector('#app'));
-    app.render();
-  }
-});
-appMainBtn.addEventListener('click', () => {
-  history.pushState({}, '', '/');
-  if (document.querySelector('.about')) {
-    document.querySelector('.about').remove();
-    const app = new App(document.querySelector('#app'));
-    app.render();
-  }
-});
